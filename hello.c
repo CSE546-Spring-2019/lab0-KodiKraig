949ffdf59b6900b394f04bb1903e8929771e2aa2
Cody Craig <cody.craig@aol.com>
2019-01-08 09:44:12 -0500

    Change to print my name

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
+/**
+ * @Author: Cody Craig <codycraig>
+ * @Date:   08-Jan-2019, 9:41:52 am
+ * @Email:  cody.craig500@gmail.com
+ * @Filename: hello.c
+ * @Last modified by:   codycraig
+ * @Last modified time: 08-Jan-2019, 9:43:09 am
+ */
+
+
+
 #include <stdio.h>
 int main()
 {
-  printf ("Hello world\n");
+  printf ("Hello world I am Cody.\n");
   return 0;
   }
